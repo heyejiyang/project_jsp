944a75aff3390663d4a2491fed76e04f873eeb31
heyejiyang <xoxo99312@naver.com>
2024-06-26 20:50:03 +0900

    관리자 게시판 - 기본구성

--- boardPractice/src/main/java/org/choongang/admin/advices/AdminControllerAdvice.java
+++ boardPractice/src/main/java/org/choongang/admin/advices/AdminControllerAdvice.java
@@ -1,5 +1,6 @@
 package org.choongang.admin.advices;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.choongang.global.Interceptor;
 import org.choongang.global.config.annotations.ControllerAdvice;
@@ -7,18 +8,60 @@ import org.choongang.global.config.annotations.ModelAttribute;
 import org.choongang.global.exceptions.UnAuthorizedException;
 import org.choongang.member.MemberUtil;
 
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @RequiredArgsConstructor
 @ControllerAdvice("org.choongang.admin")
 public class AdminControllerAdvice implements Interceptor {
 
     private final MemberUtil memberUtil;
+    private final HttpServletRequest request;
 
     @Override
     public boolean preHandle() {
         if(!memberUtil.isAdmin()) {
-            throw new UnAuthorizedException();
+            //throw new UnAuthorizedException();
         }
         return true; //true이면 관리자 화면 보이고 false이면 안보임
         //관리자가 아니면 예외발생되도록할거임
+    }
+
+    /**
+     * 서브메뉴
+     * @return
+     */
+    @ModelAttribute
+    public Map<String, List<String[]>> subMenusAll(){
+        Map<String, List<String[]>> menus = new HashMap<>();
+
+        /* 게시판 관리 서브 메뉴 S */
+        List<String[]> boardMenus = new ArrayList<>();
+        boardMenus.add(new String[]{"게시판 목록","/admin/board"});
+        boardMenus.add(new String[]{"게시판 등록","/admin/board/register"});
+        boardMenus.add(new String[]{"게시글 관리","/admin/board/posts"});
+        menus.put("board", boardMenus);
+        /* 게시판 관리 서브 메뉴 E */
+        return menus;
+    }
+
+    /**
+     * 주 메뉴 코드 - /admin/주메뉴코드
+     * @return
+     */
+    @ModelAttribute
+    public String menuCode(){
+        String uri = request.getRequestURI().replace(request.getContextPath(),"");
+        Pattern pattern = Pattern.compile("^/admin/([^/]+)/?");
+        Matcher matcher = pattern.matcher(uri);
+        return matcher.find() ? matcher.group(1) : "";
+    }
+
+    @ModelAttribute
+    public List<String[]> subMenus(){
+        Map<String,List<String[]>> menus = subMenusAll();
+        String code = menuCode();
+        return menus.get(code);
     }
 }
